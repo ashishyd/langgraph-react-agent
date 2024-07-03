2474d9561e22f2cb8a6506f2ec0fce1116e034b9
ASHISH YADAV <ashish.yd@gmail.com>
2024-07-03 12:31:07 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from langgraph.constants import END
+from langgraph.graph import StateGraph
+from langchain_core.agents import AgentFinish
+from nodes import execute_tools, run_agent_reasoning_engine
+from state import AgentState
+
+load_dotenv()
+
+AGENT_REASON = "agent_reason"
+ACT = "act"
+
+
+def should_continue(state: AgentState) -> str:
+    if isinstance(state["agent_outcome"], AgentFinish):
+        return END
+    else:
+        return ACT
+
+
+flow = StateGraph(AgentState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning_engine)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, execute_tools)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue)
+
+flow.add_edge(ACT, AGENT_REASON)
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
+
+if __name__ == "__main__":
+    print("ReAct")
+    res = app.invoke(input={
+        "input": " What is the weather in bangalore? Write it and then triple it"
+    })
+    print(res["agent_outcome"].return_values["output"])
